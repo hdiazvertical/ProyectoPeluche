43f011327a88fedfc32d87b879ac539ecc9ccc70
hdiazvertical <hdiaz@vertical.codaltec.com>
2018-12-13 11:30:24 -0500

    commit inicial

---
+++ EF6CodeFirstDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace EF6CodeFirstDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var studentAddress = new StudentAddress()
+                {
+                    Address1 = "Dir 1"
+                };
+                ctx.StudentAddresses.Add(studentAddress);
+
+                var student = new Student() {
+                    StudentName = "Bill",
+                    Height = 3,
+                    Address = studentAddress
+                };
+
+                ctx.Students.Add(student);
+
+                ctx.SaveChanges();
+
+                var result = ctx.Students.Where(t => t.StudentName == "Bill" && t.Height == 2).ToList();
+
+                Console.WriteLine(result.Count());
+                Console.WriteLine(result.ToString());
+            }
+
+            //using (var ctx = new SchoolContext()) {
+            //    var count = ctx.Students.Where(t => t.StudentName == "Bill").Count();
+            //    Console.WriteLine(count.ToString());
+            //}
+
+            Console.WriteLine("Demo completed.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EF6CodeFirstDemo/SchoolContext.cs
@@ -0,0 +1,31 @@
+﻿using EF6CodeFirstDemo;
+using System.Data.Entity;
+
+namespace EF6CodeFirstDemo
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolDBConnectionString")
+        {
+            //Database.SetInitializer<SchoolContext>(new SchoolDBInitializer());
+            Database.SetInitializer<SchoolContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Adds configurations for Student from separate class
+            modelBuilder.Configurations.Add(new StudentConfigurations());
+
+            modelBuilder.Entity<Teacher>()
+                .ToTable("TeacherInfo");
+
+            modelBuilder.Entity<Teacher>()
+                .MapToStoredProcedures();
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentAddress> StudentAddresses { get; set; }
+    }
+}
